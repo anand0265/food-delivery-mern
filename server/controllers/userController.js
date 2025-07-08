3c56d630661035509b4be0ca253e67b37688c1f7
anand0265 <156602937+anand0265@users.noreply.github.com>
2025-07-08 12:45:53 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
 
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const validator = require('validator')
 const UserModel = require('../models/UserModel')
 
